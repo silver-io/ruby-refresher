03875d24943f66bbf7ce2fd3ab2e3fd41dbd16dc
javier <js@silveriofunk.com>
2014-09-28 22:42:05 +0100

    second test passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,3 +1,7 @@
 def select_elements_starting_with_a(array)
   array.select {|first_letter| first_letter[0] == 'a'}
 end
+
+def select_elements_starting_with_vowel(names)
+    names.select {|vowels| ['a', 'e', 'i', 'o', 'u'].include?(vowels[0])}
+end
